a0ff53820d55174ef4887a70c47617ecafbf2308
Theo Caldas <theocaldas28@gmail.com>
2024-02-26 21:22:44 -0300

    Add total position to home page

--- public/view/home/home.js
+++ public/view/home/home.js
@@ -1,22 +1,40 @@
 import { fetchStocks } from '../../controller/homeController.js';
 
 onload = () => {
-    updateTable();
+    updateHomeUI();
 }
 
-function updateTable(){
-    const table = document.getElementById('stocks');
+/*  Fetches user stocks and updates view. */
+function updateHomeUI(){
     fetchStocks().then((stocks) => {
         console.log(stocks);
-        stocks.forEach(stock => {
-            table.innerHTML += `
-                <tr>
-                    <td>${stock.ticker}</td>
-                    <td>${stock.shares}</td> 
-                    <td>${stock.position}</td>
-                </tr>
-            `;
-        });
+        updateTable(stocks);
+        updateTotal(stocks);
     });
+}
+
+/*  Updates table with stocks array */
+function updateTable(stocks){
+    const table = document.getElementById('stocks');
+    stocks.forEach(stock => {
+        table.innerHTML += `
+            <tr>
+                <td>${stock.ticker}</td>
+                <td>${stock.shares}</td> 
+                <td>${stock.position}</td>
+            </tr>
+        `;
+    });
+};
+
+/*  Updates total position with stocks array */
+function updateTotal(stocks){
+    const pos = document.getElementById('total');
+    var sum = 0.00;
+    stocks.forEach(stock => {
+        sum += parseFloat(stock.position);
+    });
+    const label = sum.toFixed(2).replace('.', ',');
+    pos.innerHTML = `R$ ${label}`;
 }
 
