90c9aa320228da098dcf7acac358f2d381815495
Theo Caldas <theocaldas28@gmail.com>
2024-02-28 12:37:20 -0300

    Add bootstrap to purchase page

--- public/view/purchase/purchase.js
+++ public/view/purchase/purchase.js
@@ -3,26 +3,41 @@ import { tickerIsValid } from '../../service/stockService.js';
 import { validatePurchaseInput, computePosition, formatNumber } from '../../utils.js';
 
 onload = () => {
-    const button = document.getElementById('buy');
-    button.onclick = buyStock;
+    document.getElementById('buy').onclick = buyStock;
+    document.getElementById("tickerBtn").onclick = async () => {
+        if (await checkTicker()){
+            //should collapse only on first click
+            document.getElementById("collapseControl").click();
+            const bottom = document.getElementById("bottom");
+            if (bottom) bottom.id = "bottomCollapsed";
+        }
+    };
+
+    //reset feedbacks if any of this elements are on focus
+    resetFeedbackOnFocus([
+        document.getElementById("ticker"),
+        document.getElementById("shares"),
+        document.getElementById("price")
+    ]);
 }
 
 /*  Gets purchase user input on the client-side.
     Checks if input is valid and sends it to the server. */
 async function buyStock(){
-    const ticker = document.getElementById("ticker").value.toUpperCase();
-    const shares = document.getElementById("shares").value;
-    const pricePerShare = document.getElementById("price").value;
+    const tickerInput = document.getElementById("ticker");
+    const sharesInput = document.getElementById("shares");
+    const priceInput = document.getElementById("price");
+
+    const ticker = tickerInput.value.toUpperCase();
+    const shares = sharesInput.value;
+    const pricePerShare = priceInput.value;
 
     const errors = validatePurchaseInput(ticker, shares, pricePerShare);
     if (errors == undefined){
         // proceed with purchase. 
         try{
             //check if ticker is valid
-            if (! await tickerIsValid(ticker)){
-                console.log("Invalid ticker!");
-                return;
-            }
+            await checkTicker()
             
             //compute final price
             const total = computePosition(shares, pricePerShare);
@@ -49,8 +64,45 @@ async function buyStock(){
             console.error(error);
         }
     } else{
-        if (errors.ticker) console.log("Ticker should not be empty!");
-        if (errors.shares) console.log("Shares should be a positive number!");
-        if (errors.pricePerShare) console.log("Price per share should be a positive decimal number!");
+        if (errors.ticker) setValidElement(tickerInput, false);
+        if (errors.shares) setValidElement(sharesInput, false);
+        if (errors.pricePerShare) setValidElement(priceInput, false);
     }
 };
+
+function resetFeedbackOnFocus(elements){
+    for (const element of elements){
+        element.onfocus = () => {
+            for (const element2 of elements){
+                element2.classList.remove("is-invalid");
+                element2.classList.remove("is-valid");
+            }
+        }
+    }
+}
+
+async function checkTicker(){
+    const tickerInput = document.getElementById("ticker");
+    const ticker = tickerInput.value.toUpperCase();
+
+    console.log(ticker)
+
+    //check if ticker is valid
+    if (! await tickerIsValid(ticker)){
+        console.log("Invalid ticker!");
+        setValidElement(tickerInput, false);
+        return false;
+    }
+    setValidElement(tickerInput, true);
+    return true;
+};
+
+function setValidElement(element, isValid){
+    element.classList.remove("is-valid");
+    element.classList.remove("is-invalid");
+    if (isValid){
+        element.classList.add("is-valid");
+    }else{
+        element.classList.add("is-invalid");
+    }
+}
